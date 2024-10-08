48c5a33cbdfe68e88ab39d938fc3cb1c08a1207c
piierro <nikitagorbachovbar@yandex.ru>
2024-10-08 19:14:57 +0400

    update sellerpanel, header, tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,42 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Tabs.css';
 import TopUpIcon from '../../assets/icons/top-up.svg';
 import CartIcon from '../../assets/icons/cart.svg';
 import PersonIcon from '../../assets/icons/person.svg';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+
+import profile_icon from '../../assets/img/tabs/profile_icon.svg'
+import my_purchases_icon from '../../assets/img/tabs/my_purchases_icon.svg'
+import rules_icon from '../../assets/img/tabs/rules_icon.svg'
+import faq_icon from '../../assets/img/tabs/faq_icon.svg'
+import support_icon from '../../assets/img/tabs/support_icon.svg'
+import become_a_seller_icon from '../../assets/img/tabs/become_a_seller_icon.svg'
+import logout_icon from '../../assets/img/tabs/logout_icon.svg'
 
 const Tabs = () => {
 
-  return (
-  <div className='content-tabs'>
-  <h3 className='titleHeader'>
-    Balance: <span className='spanTitle'>$300</span>
-  </h3>
-  <div className='tab-buttons'>
-    
-    <Link to="/topup" className='btnHeader' aria-label="Top up balance">
-      Top up
-      <img src={TopUpIcon} alt="Top up" className='icon' />
-    </Link>
+    const [profileMenuDisplay, setProfileMenuDisplay] = useState('none')
 
-    <Link to="/cart" className='btnHeader' aria-label="View cart">
-       Cart 
-      <img src={CartIcon} alt="Cart" className='icon' />
-    </Link>
+    return (
+        <div>
+            <div className='content-tabs'>
+                <h3 className='titleHeader'>
+                    Balance: <span className='spanTitle'>$300</span>
+                </h3>
+                <div className='tab-buttons'>
 
-    <Link to="/profile" className='btnHeader' aria-label="View profile">
-      Profile
-      <img src={PersonIcon} alt="Profile" className='icon' />
-    </Link>
+                    <Link to="/topup" className='btnHeader' aria-label="Top up balance">
+                        Top up
+                        <img src={TopUpIcon} alt="Top up" className='icon'/>
+                    </Link>
 
-  </div>
-  
-  <div className='btnHeader-mobile'>   
-      <button className='btnHeader' aria-label="View profile">
-      <img src={PersonIcon} alt="Profile" className='icon' />
-    </button>
-  </div>
-</div>
+                    <Link to="/cart" className='btnHeader' aria-label="View cart">
+                        Cart
+                        <img src={CartIcon} alt="Cart" className='icon'/>
+                    </Link>
+
+                    <p className='btnHeader' aria-label="View profile" onClick={() => setProfileMenuDisplay('flex')}>
+                        Profile
+                        <img src={PersonIcon} alt="Profile" className='icon'/>
+                    </p>
+
+                </div>
+
+                <div className='btnHeader-mobile'>
+                    <button className='btnHeader' aria-label="View profile"
+                            onClick={() => setProfileMenuDisplay('flex')}>
+                        <img src={PersonIcon} alt="Profile" className='icon'/>
+                    </button>
+                </div>
+            </div>
+
+            <div className="profile-menu" style={{display: profileMenuDisplay}}>
+                <div className="profile-menu-background" onClick={() => setProfileMenuDisplay('none')}></div>
+                <div className="profile-menu-content">
+                    <Link to="/profile">
+                        <p>Profile</p>
+                        <img src={profile_icon}/>
+                    </Link>
+                    <Link to="/purchase">
+                        <p>My Purchases</p>
+                        <img src={my_purchases_icon}/>
+                    </Link>
+                    <Link to="/rules">
+                        <p>Rules</p>
+                        <img src={rules_icon}/>
+                    </Link>
+                    <Link to="/faq">
+                        <p>FAQ</p>
+                        <img src={faq_icon}/>
+                    </Link>
+                    <Link to="/support">
+                        <p>Support</p>
+                        <img src={support_icon}/>
+                    </Link>
+                    <Link to="/#">
+                        <p>Become a Seller</p>
+                        <img src={become_a_seller_icon}/>
+                    </Link>
+                    <Link to="/logout">
+                        <p>Logout</p>
+                        <img src={logout_icon}/>
+                    </Link>
+                </div>
+            </div>
+        </div>
   );
 };
 
--- src/pages/SellerPanel/SellerPanel.jsx
+++ src/pages/SellerPanel/SellerPanel.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import {CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 import './SellerPanel.page.css'
 import Header from "../../components/Header/Header";
 import SiteButton from "../../components/UI/inputs/SiteButton";
 
 const SellerPanel = () => {
+
+    const data = [
+        {
+            name: '28.09',
+            profit: 15,
+        },
+        {
+            name: '29.09',
+            profit: 45,
+        },
+        {
+            name: '30.09',
+            profit: 76,
+        },
+        {
+            name: '01.10',
+            profit: 54,
+        },
+        {
+            name: '02.10',
+            profit: 43,
+        },
+        {
+            name: '03.10',
+            profit: 86,
+        },
+        {
+            name: '04.10',
+            profit: 65,
+        },
+    ];
+
+    const CustomTooltip = ({active, payload, label}) => {
+        if (active) {
+            return (
+                <div className="custom-tooltip">
+                    <p>{`${payload[0].value}`}</p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+
     return (
         <div>
-            <Header />
+            <Header/>
             <div className="sellerPanel_form">
 
                 <div className="sellerPanel_title">
@@ -26,13 +71,33 @@ const SellerPanel = () => {
                             <p>Sales chart</p>
                         </div>
                         <div className="sellerCart_buttons">
-                            <SiteButton value="In a week" />
-                            <SiteButton value="Per month" />
-                            <SiteButton value="For all time" />
+                            <SiteButton value="In a week"/>
+                            <SiteButton value="Per month"/>
+                            <SiteButton value="For all time"/>
                         </div>
                     </div>
                     <div className="sellerCart_content">
-                        <p>Graphs are usually output using plugins</p>
+
+                        <ResponsiveContainer width="95%" height={400}>
+                            <LineChart
+                                width={500} height={300}
+                                data={data}
+                                margin={{
+                                    top: 5,
+                                    right: 30,
+                                    left: 20,
+                                    bottom: 5,
+                                }}>
+                                <CartesianGrid strokeDasharray="3 3"/>
+                                <XAxis dataKey="name"/>
+                                <YAxis/>
+                                <Tooltip
+                                    content={<CustomTooltip/>}
+                                />
+                                <Line type="dashed" dataKey="profit" stroke="#171826" activeDot={{r: 12}}/>
+                            </LineChart>
+                        </ResponsiveContainer>
+
                     </div>
                 </div>
 
