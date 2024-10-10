b7ca929fe143d0e52a81741e3b85d598bfa57f22
piierro <nikitagorbachovbar@yandex.ru>
2024-10-10 18:46:35 +0400

    update purchase

--- src/pages/Purchase/PurchaseBlock/PurchaseBlock.jsx
+++ src/pages/Purchase/PurchaseBlock/PurchaseBlock.jsx
@@ -14,93 +14,33 @@ const PurchaseBlock = () => {
 
     const {user} = useContext(Context)
 
+    const [totalPrice, setTotalprice] = useState(0)
+    const [totalQuantity, setTotalQuantity] = useState(0)
+
     const [loading, setLoading] = useState(true)
     const [purchasesData, setPurchasesData] = useState(<p>No purchases have been made at the moment</p>)
     const [productData, setProductData] = useState('')
 
     const getPurchasesListFunction = async () => {
 
-        const response = [
-            {
-                "uuid": '8a53a159-f9d5-4052-af75-0e7d565febfc',
-                "user": '2a53a159-f9d5-4012-af75-0e7d555febzz',
-                "purchase_time": "2024-05-03T03:11:13.838720",
-                "paid_amount": 0.0,
-                "quantity": 1,
-                "refunded_quantity": 1,
-                "category": 'category',
-                "country": 'country',
-                "bin": 541123
-            },
-            {
-                "uuid": '3a53a129-f9d5-5052-a8i5-0e7d565oekfe',
-                "user": '2a53a159-f9d5-4012-af75-0e7d555febzz',
-                "purchase_time": "2024-05-03T03:11:13.838720",
-                "paid_amount": 0.0,
-                "quantity": 2,
-                "refunded_quantity": 0,
-                "category": 'category',
-                "country": 'country',
-                "bin": 541124
-            }
-        ] //await user.getPurchasesList()
+        const response = await user.getPurchasesList()
 
         return response
     }
 
     const [isModalOpen, setModalOpen] = useState(false)
 
-    const handleCardClick = (title, description, uuid) => {
+    const handleCardClick = (title, description, uuid, quantity, price) => {
 
-        const response = {
-            'was_missing': false,
-            'positions': [
-                {
-                    "uuid": "123fd21d-7216-4e50-b91d-fb625d95d0c7",
-                    "category": "high-valid-cc-no-refund-3",
-                    "country": "united-states",
-                    "data": "CC_STRING",
-                    "seller": "90hf0ce0-48f9-4019-97dc-1edb530dad33",
-                    "upload": "654dcb3c-5996-406d-801e-fb6ae6f347ea",
-                    "bin": 524452,
-                    "is_loaded": true,
-                    "purchase": "9hg2e55f-c2c8-4dd2-aed1-8f58cb5a7608",
-                    "is_refunded": false,
-                    "is_purchased": true,
-                    "buyer": "0f456e68-6566-4796-a4b6-9182b5abadb1"
-                },
-                {
-                    "uuid": "321b084c-c296-48c5-a990-9c1dd8a67a3b",
-                    "category": "high-valid-cc-no-refund-3",
-                    "country": "japan",
-                    "data": "CC_STRING",
-                    "seller": "90hf0ce0-48f9-4019-97dc-1edb530dad33",
-                    "upload": "654dcb3c-5996-406d-801e-fb6ae6f347ea",
-                    "bin": 461676,
-                    "is_loaded": true,
-                    "purchase": "768b9c88-3e2b-450e-b598-95b78ebb4523",
-                    "is_refunded": false,
-                    "is_purchased": true,
-                    "buyer": "of456e68-6566-4796-a4b6-9182b5abadb1"
-                }
-            ],
-            'purchase': {
-                "uuid": '8a53a159-f9d5-4052-af75-0e7d565febfc',
-                "user": '2a53a159-f9d5-4012-af75-0e7d555febzz',
-                "purchase_time": "2024-05-03T03:11:13.838720",
-                "paid_amount": 0.0,
-                "quantity": 1,
-                "refunded_quantity": 1,
-                "category": 'category',
-                "country": 'country',
-                "bin": 541123
-            }
-        } //user.verifyPositionInPurchareByPurchare(uuid)
+        const response = user.getPositionByPurchare(uuid)
 
         if (Object.keys(response).length !== 0) {
 
-            setProductData(response['positions'].map(item =>
-                <React.Fragment key={item.id}>
+            setTotalprice(price)
+            setTotalQuantity(quantity)
+
+            setProductData(response.map((item, index) =>
+                <React.Fragment key={index + 1}>
                     <div className='product-card'>
                         <div className='prod-block'>
                             <div className='purchase-img'></div>
@@ -108,10 +48,10 @@ const PurchaseBlock = () => {
                         </div>
                         <div className='purchase-details'>
                             <div className='purchase-price'>
-                                <span className='purchase-cash'>{item.paid_amount}</span>
+                                <span className='purchase-cash'></span>
                             </div>
                             <div className='purchase-quantity'>
-                                <span className='quantity'>0</span>
+                                <span className='quantity'></span>
                             </div>
                         </div>
                     </div>
@@ -152,7 +92,7 @@ const PurchaseBlock = () => {
                             bin={item.bin}
                             date={item.purchase_time}
                             uuid={item.uuid}
-                            onClick={() => handleCardClick(item.title, item.description, item.uuid)}
+                            onClick={() => handleCardClick(item.title, item.description, item.uuid, item.quantity, item.paid_amount)}
                         />
                     ))
 
@@ -188,7 +128,7 @@ const PurchaseBlock = () => {
                 {productData}
 
                 <div className='purchase-footer'>
-                    <div className='discount-block'>
+                    <div className='discount-block' style={{display: 'none'}}>
                         <p className='discount-title'>Discount</p>
                         <span className='discount-cash'>-10%</span>
                     </div>
@@ -196,8 +136,8 @@ const PurchaseBlock = () => {
                     <div className='discount-block total-container'>
                         <p>Total</p>
                         <div className='total-block'>
-                            <span className='purchase-total'>45$</span>
-                            <span className='quantity'>8</span>
+                            <span className='purchase-total'>{totalPrice}$</span>
+                            <span className='quantity'>{totalQuantity}</span>
                         </div>
                     </div>
                 </div>
